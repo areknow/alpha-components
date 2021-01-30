474042b48c9a13d89a86371cbb90a476198bf1d4
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-01-30 12:30:14 -0500

    added button scaffold

---
+++ libs/react/button/src/lib/react-button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './react-button.module.scss';
+
+/* eslint-disable-next-line */
+export interface ReactButtonProps {}
+
+export function ReactButton(props: ReactButtonProps) {
+  return (
+    <div>
+      <h1>Welcome to react-button!</h1>
+    </div>
+  );
+}
+
+export default ReactButton;
