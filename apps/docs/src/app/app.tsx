31bb37b9fc672e5ca34de2b42329a2e46dd31551
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-03-05 13:59:08 -0500

    dynamic nav states

--- apps/docs/src/app/app.tsx
+++ apps/docs/src/app/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import styles from './app.module.scss';
 import Nav from './components/nav/nav';
-import SideMenu from './components/nav/side-menu/side-menu';
+import SideMenu from './components/side-menu/side-menu';
 import ButtonPage from './pages/button';
 import RadioPage from './pages/radio';
 
--- apps/docs/src/app/components/nav/nav.tsx
+++ apps/docs/src/app/components/nav/nav.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ReactComponent as Logo } from '../../../assets/logo.svg';
 import { ReactComponent as Title } from '../../../assets/title.svg';
 import styles from './nav.module.scss';
 
 export const Nav = () => {
+  const [scrolledNav, toggleScrolledNav] = useState(false);
+  const [expandNav, toggleExpandNav] = useState(false);
+  const navClasses = [
+    scrolledNav ? styles.scrolled : null,
+    scrolledNav && expandNav ? styles.expanded : null,
+  ].join(' ');
+
+  useEffect(() => {
+    const fireOnScroll = () => {
+      toggleScrolledNav(window.scrollY > 90);
+    };
+    window.addEventListener('scroll', fireOnScroll, true);
+    return () => window.removeEventListener('scroll', fireOnScroll, true);
+  });
+
   return (
-    <nav>
+    <nav
+      className={navClasses}
+      onMouseEnter={() => toggleExpandNav(true)}
+      onMouseLeave={() => toggleExpandNav(false)}
+    >
       <div className={styles.brand}>
         <Logo />
         <Title />
@@ -16,6 +35,9 @@ export const Nav = () => {
           <li>Theme</li>
           <li>FAQ</li>
         </ul>
+      </div>
+      <div className={styles.mobileTrigger}>
+        <Logo />
       </div>
     </nav>
   );
