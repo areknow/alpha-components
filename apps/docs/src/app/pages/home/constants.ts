98dc084a8c9717988506e12bd6f7fd7d7bd7a320
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-03-30 14:42:26 -0400

    docs: font example

--- apps/docs/src/app/pages/home/constants.ts
+++ apps/docs/src/app/pages/home/constants.ts
@@ -1,7 +1,11 @@
 export const INSTALL_CODE = `$ npm install --save @miniml/alpha-components-react 
 $ yarn add @miniml/alpha-components-react`;
 
-export const FONT_CODE = `@import url('https://indestructibletype.com/fonts/Jost.css');`;
+export const FONT_CODE = `@import url('https://indestructibletype.com/fonts/Jost.css');
+
+body {
+  font-family: "Jost";
+}`;
 
 export const USAGE_CODE = `import { Root } from '@miniml/alpha-components-react/root';
 import { Button } from '@miniml/alpha-components-react/button';
