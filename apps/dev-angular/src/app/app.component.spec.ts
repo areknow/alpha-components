2dc7e4cab6031ba5fe6d60ebb2890ec981fac267
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-04-02 14:29:10 -0400

    workspace: test

--- apps/dev-angular/src/app/app.component.spec.ts
+++ apps/dev-angular/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AlphaButtonModule } from '@miniml/alpha-components-angular/button';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, AlphaButtonModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -14,20 +15,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'dev-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dev-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to dev-angular!'
-    );
   });
 });
