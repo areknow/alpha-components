b404496e1a0003887d7de85ba4ef951ece16f2d0
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-04-02 11:08:13 -0400

    checkbox: added unit tests

--- libs/react/checkbox/src/lib/react-checkbox.spec.tsx
+++ libs/react/checkbox/src/lib/react-checkbox.spec.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
+import Checkbox from './react-checkbox';
 
-import ReactCheckbox from './react-checkbox';
-
-describe('ReactCheckbox', () => {
+describe('Checkbox', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ReactCheckbox />);
+    const { baseElement } = render(<Checkbox label="foo" value="bar" />);
+    expect(baseElement.querySelector('.alpha-checkbox')).toBeTruthy();
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should use label property', () => {
+    const label = 'foo';
+    const { queryByText } = render(<Checkbox label={label} value="bar" />);
+    expect(queryByText(label)).toBeTruthy();
+  });
+
+  it('should use value property', () => {
+    const value = 'bar';
+    const { baseElement } = render(<Checkbox label="foo" value={value} />);
+    const valueAttr = baseElement.querySelector('input').getAttribute('value');
+    expect(valueAttr).toEqual(value);
+  });
+
+  it('should be checked by default', () => {
+    const { baseElement } = render(
+      <Checkbox checked label="foo" value="bar" />
+    );
+    const checked = baseElement.querySelector('input').getAttribute('checked');
+    expect(checked).not.toEqual(null);
+  });
+
+  it('should not be checked by default', () => {
+    const { baseElement } = render(<Checkbox label="foo" value="bar" />);
+    const checked = baseElement.querySelector('input').getAttribute('checked');
+    expect(checked).toEqual(null);
   });
 });
